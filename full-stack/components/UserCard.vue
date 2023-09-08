19f3a21c6694fb72d90d16ce768043dfe445fc8b
Jamie Conway <jamie@jpconway.com>
2023-09-08 10:31:01 -0500

    remove city from card and refine style

--- full-stack/components/UserCard.vue
+++ full-stack/components/UserCard.vue
@@ -13,14 +13,14 @@ const props = defineProps({
 
 <template>
 	<div
-		class="relative flex flex-row gap-4 items-baseline justify-between border border-gray-600 rounded-md p-4 shadow-md hover:bg-gray-100 transition-all overflow-hidden"
+		class="relative flex flex-row gap-4 items-baseline justify-between border border-gray-300 hover:border-gray-100 rounded-md p-4 shadow-md hover:bg-gray-100 transition-all overflow-hidden"
 	>
 		<span class="flex flex-row gap-4 items-start">
-			<img :src="user?.picture.thumbnail" />
-			<div>{{ user?.name.first }} {{ user?.name.last }}</div>
+			<img class="rounded-md" :src="user?.picture.thumbnail" />
+			<div class="text-2xl">{{ user?.name.first }} {{ user?.name.last }}</div>
 		</span>
 		<div class="absolute bottom-2 right-4 text-sm text-gray-500 text-right">
-			{{ user?.location.city }}, {{ user?.location.state }},
+			{{ user?.location.state }},
 			{{ user?.location.country }}
 		</div>
 	</div>
